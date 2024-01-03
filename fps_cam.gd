09a5186f2814ab542ec4785f65d6867bcf4ce284
Layton <laytonb@hotmail.co.uk>
2024-01-03 14:25:13 +0000

    placing pieces working
    nextup: intersection detection and refusal

--- fps_cam.gd
+++ fps_cam.gd
@@ -1,39 +1,28 @@
 class_name FPSController
 extends Camera3D
 
-
-class ClickData:
-    var normal: Vector3
-    var click_pos: Vector3
-
-    func _init(normal: Vector3, click_pos: Vector3) -> void:
-        self.normal = normal
-        self.click_pos = click_pos
-
-    func get_pos_cam_side() -> Vector3:
-        return click_pos + normal
-
-    func get_pos_obj_side() -> Vector3:
-        return click_pos - normal
-
-
 @export_group("Controls")
 @export var move_speed := 3
 @export var move_sprint_multiplier := 2
 
 var _pivot := Vector3.ZERO
-var _preview_container: MeshInstance3D
+var _preview: MeshInstance3D
 var _preview_offset: Vector2i = Vector2i(0, 0)
+var _preview_rotation: int = 0
 var _last_coord: Vector3
 
-@onready var building_ui := $"../../BuildingUi" as BuildingUi
-@onready var gridmap := $"../GridMap" as BuildingGrid
+@onready var _building_ui := $"../../BuildingUi" as BuildingUi
+@onready var _preview_container := $"../PreviewContainer" as Node3D
+@onready var _gridmap := $"../BuildingGrid" as BuildingGrid
 
 func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_pressed("select"):
         var intersect_data := get_intersect_data()
-        if !intersect_data.is_empty() and building_ui.selected != -1 and _last_coord:
+        if !intersect_data.is_empty() and _building_ui.selected != -1 and _last_coord:
             _instantiate_preview()
+    elif event.is_action_pressed("rotate"):
+        _preview_rotation = (_preview_rotation + 1) % 4
+        _preview.rotate_y(PI / 2)
     elif event.is_action_pressed("increase_offset_alt"):
         _preview_offset.y += 1
     elif event.is_action_pressed("decrease_offset_alt"):
@@ -66,15 +55,15 @@ func _apply_movement(delta: float) -> void:
     look_at(_pivot)
 
 func _show_placement_preview() -> void:
-    if building_ui.selected != -1:
+    if _building_ui.selected != -1:
         var intersection_data = get_intersect_data()
         if intersection_data.is_empty():
-            _preview_container.visible = false
+            _preview.visible = false
         else:
             var pos: Vector3 = intersection_data["position"]
-            var normal := intersection_data["normal"] as Vector3
+            var normal: Vector3 = intersection_data["normal"]
             pos = _shift_pos_using_selected(normal, pos)
-            _last_coord = gridmap.local_to_map(pos)
+            _last_coord = _gridmap.local_to_map(pos)
             _show_preview_at_coord()
 
 func _shift_pos_using_selected(normal: Vector3, pos: Vector3) -> Vector3:
@@ -85,7 +74,9 @@ func _shift_pos_using_selected(normal: Vector3, pos: Vector3) -> Vector3:
         normals = [Vector3(1, 0, 0), Vector3(0, 1, 0)]
     else:
         normals = [Vector3(0, 0, 1), Vector3(1, 0, 0)]
-    var half_mesh_size := _preview_container.mesh.get_aabb().size / 2
+    var half_mesh_size := _preview.mesh.get_aabb().size / 2
+    if _preview_rotation % 2 == 1: # adjust sizes to match rotation
+        half_mesh_size = Vector3(half_mesh_size.z, half_mesh_size.y, half_mesh_size.x)
     _clamp_preview_offset(normals, half_mesh_size)
     pos += normal * half_mesh_size # move preview center to align
     pos += normal.abs() * .1 # fix exact-grid-border issues
@@ -100,11 +91,13 @@ func _clamp_preview_offset(normals: Array[Vector3], half_mesh_size: Vector3) -> 
     _preview_offset.y = clamp(_preview_offset.y, -max_offsets.y + 1, max_offsets.y)
 
 func _show_preview_at_coord() -> void:
-    _preview_container.visible = true
-    _preview_container.transform.origin = _last_coord
+    _preview.visible = true
+    _preview.transform.origin = _last_coord
 
 func _instantiate_preview() -> void:
-    gridmap.set_cell_item(_last_coord, building_ui.selected)
+    var rotation_integer := _gridmap.get_orthogonal_index_from_basis(
+        Basis(Vector3(0, 1, 0), PI * _preview_rotation / 2))
+    _gridmap.set_cell_item(_last_coord, _building_ui.selected, rotation_integer)
 
 func get_intersect_data() -> Dictionary:
     var mouse_position := get_viewport().get_mouse_position()
@@ -114,23 +107,24 @@ func get_intersect_data() -> Dictionary:
     return worldspace.intersect_ray(PhysicsRayQueryParameters3D.create(ray_start, ray_end, 0b10))
 
 func _on_building_ui_item_selected(id: int) -> void:
-    if _preview_container:
+    if _preview:
         if id == -1:
-            _preview_container.queue_free()
-            _preview_container = null
+            for child in _preview_container.get_children():
+                child.queue_free()
+            _preview = null
         else:
-            _preview_container.mesh = gridmap.get_item_mesh(id)
+            _preview.mesh = _gridmap.get_item_mesh(id)
     elif id != -1:
         var mesh_node := MeshInstance3D.new()
-        mesh_node.mesh = gridmap.get_item_mesh(id)
+        mesh_node.mesh = _gridmap.get_item_mesh(id)
         var material := StandardMaterial3D.new()
         material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
         material.albedo_color = Color(1, 0, 0, 0.4)
         mesh_node.material_override = material
-        _preview_container = mesh_node
-        _preview_container.visible = false
-        _preview_container.visibility_changed.connect(_reset_preview_offset)
-        get_parent().add_child(_preview_container)
+        _preview = mesh_node
+        _preview.visible = false
+        _preview.visibility_changed.connect(_reset_preview_offset)
+        _preview_container.add_child(_preview)
 
 func _reset_preview_offset():
     _preview_offset = Vector2i(0, 0)
